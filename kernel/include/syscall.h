691e1ccf4890aaf8b79f10b18dac8c175e3c1e42
MakhOS Developer <dev@makos.dev>
2026-03-05 03:09:40 -0500

    feat(syscall): Add syscall header with syscall numbers and macros
    Define system call numbers:
    - SYS_EXIT (0), SYS_WRITE (1), SYS_READ (2)
    - SYS_OPEN (3), SYS_CLOSE (4), SYS_GETPID (5)
    - SYS_SLEEP (6), SYS_GETTICKS (7)
    
    Add syscall function type and convenience macros:
    - syscall0, syscall1, syscall2, syscall3
    - syscall_init() and syscall_handler() declarations
    
    Maximum 64 syscalls supported.

---
+++ kernel/include/syscall.h
@@ -0,0 +1,40 @@
+#ifndef MAKHOS_SYSCALL_H
+#define MAKHOS_SYSCALL_H
+
+#include <types.h>
+
+// System call numbers
+#define SYS_EXIT        0
+#define SYS_WRITE       1
+#define SYS_READ        2
+#define SYS_OPEN        3
+#define SYS_CLOSE       4
+#define SYS_GETPID      5
+#define SYS_SLEEP       6
+#define SYS_GETTICKS    7
+
+// Maximum number of syscalls
+#define MAX_SYSCALLS    64
+
+// Syscall function type
+typedef uint64_t (*syscall_fn_t)(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
+
+// Public API (for kernel use)
+void syscall_init(void);
+uint64_t syscall_handler(uint64_t num, uint64_t arg1, uint64_t arg2, 
+                         uint64_t arg3, uint64_t arg4, uint64_t arg5);
+
+// Convenience macros for making syscalls from kernel
+#define syscall0(num) \
+    syscall_handler(num, 0, 0, 0, 0, 0)
+
+#define syscall1(num, a1) \
+    syscall_handler(num, (uint64_t)(a1), 0, 0, 0, 0)
+
+#define syscall2(num, a1, a2) \
+    syscall_handler(num, (uint64_t)(a1), (uint64_t)(a2), 0, 0, 0)
+
+#define syscall3(num, a1, a2, a3) \
+    syscall_handler(num, (uint64_t)(a1), (uint64_t)(a2), (uint64_t)(a3), 0, 0)
+
+#endif
